82ef4edced218e892b3e488dc02b6396ba2aca8d
mzotika <mzotika@yahoo.gr>
2026-01-02 13:26:56 +0200

    feat: add lucide-react icons to project

--- src/components/ResultScreen.tsx
+++ src/components/ResultScreen.tsx
@@ -1,10 +1,14 @@
 import Button from "./Button";
 import type { ResultScreenProps } from "../types";
+import { Sparkles } from "lucide-react";
+
 
 
 const ResultScreen = ({ score, total, onRestart }: ResultScreenProps) => {
     return (
         <div className="bg-rose-50 border border-rose-200 shadow rounded-xl p-6 text-center space-y-4">
+            <Sparkles className="mx-auto text-[#edf03d]" size={40} />
+
             <h2 className="text-xl font-semibold text-gray-700">
                 Τέλος Quiz
             </h2>
